b96d21c91a2db0c260c1879e2f5415e92b4aa473
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-20 21:11:44 +0700

    feat: add PostRepository

---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package org.example.repository;
+
+import org.example.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
