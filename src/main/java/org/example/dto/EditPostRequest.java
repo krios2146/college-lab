c44cf9a284ceed62428bf5b276b87cfb1beb3e86
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-22 17:48:30 +0700

    feat: add EditPostRequest

---
+++ src/main/java/org/example/dto/EditPostRequest.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditPostRequest {
+
+    private String name;
+
+    private String text;
+}
