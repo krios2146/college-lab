4d4ed29705d9198000ad199d8eaa16234dbbe6bb
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-22 17:49:44 +0700

    feat: add `authenticate` method to the AuthenticationService

--- src/main/java/org/example/service/AuthenticationService.java
+++ src/main/java/org/example/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package org.example.service;
 
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.example.entity.User;
+import org.example.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -10,8 +13,10 @@ import javax.crypto.SecretKey;
 public class AuthenticationService {
 
     private final SecretKey key;
+    private final UserRepository userRepository;
 
-    public AuthenticationService() {
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
         key = Keys.hmacShaKeyFor("97e41d694a7fa2e66373a5e15a9603b12db8b4cdfdbd3296e90a15640a3b6202b475c7d7766c5e4ad5a5e3c99ed5f665ebcf3fd5984fa7a9e3e49edfe1372ec5".getBytes());
     }
 
@@ -20,5 +25,18 @@ public class AuthenticationService {
                 .subject(email)
                 .signWith(key)
                 .compact();
+    }
+
+    public User authenticate(String auth) {
+        JwtParser jwtParser = Jwts.parser()
+                .verifyWith(key)
+                .build();
+
+        String email = jwtParser.parseSignedClaims(auth)
+                .getPayload()
+                .getSubject();
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User was not found"));
     }
 }
