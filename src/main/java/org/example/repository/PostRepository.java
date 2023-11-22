6c7b8c362214557f548fdfa5f1fbde1200e35511
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-22 17:49:22 +0700

    feat: add `findByUser_Id` method to the PostRepository

--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -3,6 +3,10 @@ package org.example.repository;
 import org.example.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUser_Id(Long id);
 
 }
