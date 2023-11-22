f2e10a95942592595ee27b4cdb995ff447f54348
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-22 17:49:04 +0700

    feat: update User entity with `@JsonIgnore` annotation

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,5 +1,6 @@
 package org.example.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class User {
 
     private String email;
 
+    @JsonIgnore
     private String password;
 
     public User(String name, String email, String password) {
