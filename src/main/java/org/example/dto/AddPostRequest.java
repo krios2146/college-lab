804e326cee69227a4f0365c2e08dbace7654bbdb
Vladimir Kidyaev <krios2146@gmail.com>
2023-11-22 17:48:24 +0700

    feat: add AddPostRequest

---
+++ src/main/java/org/example/dto/AddPostRequest.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AddPostRequest {
+
+    private String name;
+
+    private String text;
+}
